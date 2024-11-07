9583a70bfbe4978b347c86f84563e237f3e7628e
Marinus Retra <marinus@flowonline.nl>
2024-11-07 21:24:16 +0100

    KamerGeneratie: Rotatie wordt nu veranderd
    + De kamers aan de rand van de map draaien nu met de ingang naarbinnen

--- Assets/Scripts/RoomGeneration/GridManager.cs
+++ Assets/Scripts/RoomGeneration/GridManager.cs
@@ -16,7 +16,9 @@ public class GridManager : MonoBehaviour
        {
             for (int x = 0; x < width; x++)
             {
-                GameObject currentRoom; 
+                GameObject currentRoom;
+                int[] Rotations = { 90,180,270};
+
                 var Tile = Instantiate(_emptyTilePrefab, new Vector3(x, 0, y), Quaternion.identity);
                 nodeArray[x, y] = Tile;
                 Tile.name = $"Tile {x} {y}"; //Dit is gewoon om de coords makkelijker te zien in de editor
@@ -26,20 +28,26 @@ public class GridManager : MonoBehaviour
                 if (x == this.width-1 || y == this.height-1 || y == 0 || x == 0)
                 {
                     currentRoom = RoomList[2];
+                    Tile.transform.Rotate(-90, 0, Rotations[Random.Range(0, Rotations.Length)]);
                 }
                 else
                 {
-                    currentRoom = RoomList[Random.Range(0, RoomList.Length-1)];
+                    currentRoom = RoomList[Random.Range(0, RoomList.Length - 1)];
+                    Tile.transform.Rotate(-90,0,0);
+                }
+                if (y == this.height)
+                {
+                    Tile.transform.Rotate(-90, 0, Rotations[Random.Range(0, 1)]);
                 }
 
                 Tile.GetComponent<MeshFilter>().mesh = currentRoom.GetComponent<MeshFilter>().mesh;
                 Tile.GetComponent<MeshRenderer>().materials = currentRoom.GetComponent<MeshRenderer>().materials;
                 Tile.GetComponent<MeshCollider>().sharedMesh = currentRoom.GetComponent<MeshCollider>().sharedMesh;
                 
-                Tile.transform.Rotate(-90,0,0);
+
                 Tile.transform.localScale = Vector3.one * 50;
 
-                //Maakt een checker patroon in de grid
+                //Maakt een checker patroon in de grid om de kamers net iets meer variatie te geven
                 if (y % 2 == 0 && x % 2 == 0 || y % 2 != 0 && x % 2 != 0) //Als allebei de x en y even zijn of als allebei de x en y oneven zijn maak ze zwart
                 {
                     Tile.GetComponent<MeshRenderer>().material.color = Color.black;
@@ -48,6 +56,7 @@ public class GridManager : MonoBehaviour
             }
        }
        Camera.transform.position = new Vector3((float)width / 2 -0.5f, 10, (float)height / 2 - 0.5f);
+
        return nodeArray;
     }
 
