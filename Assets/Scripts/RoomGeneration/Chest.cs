87d0ffa054a20721e858cba24e2ccc0712f8cdcc
Marinus Retra <marinus@flowonline.nl>
2024-11-08 12:06:25 +0100

    Chest
    + Chest spawns random items inside
    + Chest has opening animation

---
+++ Assets/Scripts/RoomGeneration/Chest.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    [SerializeField] GameObject[] possibleItems;
+    void Start()
+    {
+        foreach (GameObject possibleItem in possibleItems)
+        {
+            if (Random.value < 0.2)
+            { 
+               possibleItem.SetActive(true);
+            }
+        }
+    }
+}
