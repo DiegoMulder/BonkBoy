2264aff63783506ecf97016eb0c1217b937eb929
Marinus Retra <marinus@flowonline.nl>
2024-11-11 19:10:23 +0100

    Objecten toegevoegd in kamers
    + in de kamers worden nu willekeurige decoratie objecten gegenereerd

--- Assets/ObjectGenerator.cs
+++ Assets/ObjectGenerator.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 
 public class ObjectGenerator : MonoBehaviour
-{
+{//In dit script worden de prefabs bewaard die in een kamer kunnen verschijnen 
+    //Het zit in een los script omdat ik per kamer een andere lijst objecten wil hebben.
     [SerializeField] 
     private GameObject[] possibleObjects;
     public GameObject[] PossibleObjects { get { return possibleObjects; } private set { possibleObjects = value; } }
